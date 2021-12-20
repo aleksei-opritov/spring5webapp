3beab34d5bb80cba3210db2564ba7284d166978b
aleksei-opritov <hutte1spb@gmail.com>
2021-12-20 15:34:11 +0300

    Models package was removed. Added publishers to books.

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -41,20 +41,27 @@ public class BootStrapData implements CommandLineRunner {
 
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design", "123123");
+        ddd.setPublisher(publisher);
+        publisher.getBooks().add(ddd);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
         authorRepository.save(eric);
         bookRepository.save(ddd);
+        publisherRepository.save(publisher);
 
         Author rod = new Author("Rod", "Johnson");
         Book noEJB = new Book("J2EE Development without EJB", "3939459459");
+        noEJB.setPublisher(publisher);
+        publisher.getBooks().add(noEJB);
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
         authorRepository.save(rod);
         bookRepository.save(noEJB);
+        publisherRepository.save(publisher);
 
         System.out.println("Number of Books: " + bookRepository.count());
+        System.out.println("Number of Books issued by Publisher: " +publisher.getBooks().size());
     }
 }
